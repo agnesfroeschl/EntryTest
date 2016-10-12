5c828a7220d933fbc6cd7a5e57e5730ed0172052
agnesfroeschl <agnes.froeschl@gmx.at>
2016-10-12 19:42:25 +0200

    add json

--- src/main/java/ase/Solution.java
+++ src/main/java/ase/Solution.java
@@ -1,8 +1,11 @@
 package ase;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by Agnes on 10/10/16.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Solution {
 
     private Integer solution;
